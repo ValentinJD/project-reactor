40093b8bc0da81ffd8ba274c0b06cdbd4e1c078b
Valentin Kudinov <moysait1984@yandex.ru>
2025-01-21 10:26:01 +0400

    4 Компоновка и преобразование реактивных   потоков

--- reactive-app/src/main/java/ru/reactive/app/examples/Transform.java
+++ reactive-app/src/main/java/ru/reactive/app/examples/Transform.java
@@ -4,21 +4,47 @@ import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.util.function.Tuple2;
 
+import java.util.Random;
 import java.util.function.Function;
 
 @Slf4j
-public class Transform {
+public class Transform {// (1)
+    public final Function<Flux<String>, Flux<String>> logUserInfo = stream -> stream //
+            .index() // (1.1)
+            .doOnNext(tp -> // (1.2)
+                    log.info("[{}] User: {}", tp.getT1(), tp.getT2())) //
+            .map(Tuple2::getT2);
+
+    public static void main(String[] args) throws InterruptedException {
+        Transform errorHandle = new Transform();
+        errorHandle.compose();
+        Thread.sleep(4000);
+    }
+
     void transform() {
-        Function<Flux<String>, Flux<String>> logUserInfo = // (1)
-                stream -> stream //
-                        .index() // (1.1)
-                        .doOnNext(tp -> // (1.2)
-                                log.info("[{}] User: {}", tp.getT1(), tp.getT2())) //
-                        .map(Tuple2::getT2);
         // (1.3)
         Flux.range(1000, 3) // (2)
                 .map(i -> "user-" + i) //
                 .transform(logUserInfo) // (3)
                 .subscribe(e -> log.info("onNext: {}", e));
     }
+
+    void compose() {
+        Function<Flux<String>, Flux<String>> logUserInfo = (stream) -> { // (1)
+            Random random = new Random();
+            if (random.nextBoolean()) {
+                return stream
+                        .doOnNext(e -> log.info("[path A] User: {}", e));
+            } else {
+                return stream
+                        .doOnNext(e -> log.info("[path B] User: {}", e));
+            }
+        };
+
+        Flux<String> publisher = Flux.just("1", "2", "3","4") // (2)
+                .compose(logUserInfo); // (3)
+        publisher.subscribe(); // (4)
+        publisher.subscribe();
+    }
+
 }
